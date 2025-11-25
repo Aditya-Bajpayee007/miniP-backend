fcce9b06fc66ef80a181fa3449bfa9125f93605b
Aditya-Bajpayee007 <adityabajpayee7@gmail.com>
2025-11-25 20:38:13 +0530

    s

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ❌ Remove global DB connection here
 // connectDB() is now handled per-route in serverless environment
